34e9ad0f1d3607b1e09311e0e9ce40483e7d8e77
Jan Kohlbach <43587328+jankohlbach@users.noreply.github.com>
2025-08-15 20:16:20 +0200

    Initial commit

---
+++ src/scripts/utils.ts
@@ -0,0 +1,26 @@
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+export const debounce = (func: any, timeout = 300) => {
+  let timer: ReturnType<typeof setTimeout>;
+
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+export const lerp = (start: number, end: number, damping: number) =>
+  start * (1 - damping) + end * damping;
+
+export const mapRange = (
+  number: number,
+  inMin: number,
+  inMax: number,
+  outMin: number,
+  outMax: number,
+) => ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+
+export const hasHover = () =>
+  window.matchMedia('(hover: hover) and (pointer: fine)').matches;
